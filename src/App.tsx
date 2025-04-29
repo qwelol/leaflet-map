7975b5902c2c3a751836a3ed2136275c4808bd16
qwelol <argentum.tm@gmail.com>
2025-04-29 20:58:48 +0300

    fix: rm body margin

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import 'leaflet/dist/leaflet.css';
-import '@csstools/normalize.css';
+import 'modern-normalize/modern-normalize.css';
 import 'leaflet-contextmenu';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'leaflet-contextmenu/dist/leaflet.contextmenu.css';
